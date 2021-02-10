cd0e36e869f32f91e64ebfb87b43431d34632416
Lucas Carmichael <carmichael.lucas@outlook.com>
2021-02-10 00:15:33 -0300

    method sum

--- lib/calculator/calculator.rb
+++ lib/calculator/calculator.rb
@@ -1,3 +1,5 @@
 class Calculator
-  
+  def sum(*array)
+    array.reduce { |number, prox| number + prox }
+  end
 end
--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -2,6 +2,35 @@ $LOAD_PATH.unshift("{File.dirname(__FILE__)}/../lib/calculator/")
 
 require 'calculator'
 
-describe Calculator do
+RSpec.describe Calculator do
+  describe 'Sum' do
+    it '#for 2 numbers' do
+      c = Calculator.new
+      res = c.sum(5, 6)
 
+      expect(res).to eq(11)
+    end
+
+    it '#for 3 numbers' do
+      c = Calculator.new
+      res = c.sum(5, 3, 3)
+
+      expect(res).to eq(11)
+    end
+
+    it '#for 4 numbers' do
+      c = Calculator.new
+      res = c.sum(2, 3, 1, 5)
+
+      expect(res).to eq(11)
+    end
+
+    it '#for 0 number' do
+      c = Calculator.new
+      res = c.sum()
+
+      expect(res).to be(nil)
+    end
+  end
+  
 end
